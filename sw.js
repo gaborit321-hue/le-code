9c47bff3088adb2b921bc3b2826bbd5f8ce4715c
gaborit321-hue <gaborit321@gmail.com>
2026-03-31 11:14:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-self.addEventListener('fetch', e => {});
+const VERSION = 'v2'; // incrémente à chaque déploiement
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', () => {});
